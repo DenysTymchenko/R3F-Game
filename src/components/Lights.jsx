6af89308c823ce66be7aa7a82875e2dd745562f2
Denys <tymchenko.denys@gmail.com>
2023-07-17 16:22:22 +0300

    Lights now trailing the ball

--- src/components/Lights.jsx
+++ src/components/Lights.jsx
@@ -1,9 +1,21 @@
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber';
+
 export default function Lights() {
+  const dirLight = useRef();
+
+  useFrame((state) => {
+    dirLight.current.position.z = state.camera.position.z - 3; // -3 to make lights position a little bit forward, so the shadows wouldn't appear before player's eyes
+    dirLight.current.target.position.z = state.camera.position.z - 4;
+    dirLight.current.target.updateMatrixWorld();
+  })
+
   return (
     <>
       <ambientLight intensity={0.5} />
 
       <directionalLight
+        ref={dirLight}
         castShadow
         position={[4, 4, 1]}
         intensity={1.5}
